4db1a2426f5a5f9de3b5f5f8e2634aed23eb2a1c
FhantomWatcher <85093344+FarhanJuneidSiddiqui@users.noreply.github.com>
2024-11-03 19:56:02 +0600

    added some things as practice

--- simcal/main.cpp
+++ simcal/main.cpp
@@ -1,17 +1,33 @@
 #include<iostream>
+#include<cmath>
 
 using namespace std;
 
 int main()
-{
+{  //stroage for the numbera.
     int a,b,sum;
+    string first_name, last_name;
+//greeding user acording to userinput
+cout<< "please enter your name as first and last name [first name last name] : ";
+cin>>first_name;
+cin>>last_name;
+
+cout<<"welcome,"<<" "<< first_name << " "<< last_name<< "!" << endl; 
 
 
+  //inputs and warings for users
     cout<< "Enter your number and 1st is grater then the 2nd!"<< endl;
     cout<< "enter the 1st number : ";
     cin>>a; 
-    cout<< "you entered"<< " "<< a <<" "<<"as your 1st number!";
+    cout<< "enter the 2nd number : ";
+    cin>>b;
+  //verify messeages for user
+    cout<< "you entered"<< " "<< a <<" "<<"as your 1st number"<< " and "<< b <<" "<<"as your 2nd number!"<< endl;
+ //sum culculation
+ sum=(a+b);
 
+ cout<<"loading........"<<endl;
+ cout<<"and the answar is"<<" "<< sum <<" "<< endl;
 
     return 0;
 }
