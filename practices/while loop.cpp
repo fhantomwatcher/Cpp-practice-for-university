de1800291d10de2d461377785e066f5e73dcfa7c
FhantomWatcher <85093344+FarhanJuneidSiddiqui@users.noreply.github.com>
2024-11-11 23:51:53 +0600

    while loop test move to practice!

---
+++ practices/while loop.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+      int i=1;
+    while(i<=5){
+        cout<<"what the f***! while is work or not!"<<endl;
+        i++;
+    }
+    return 0;
+}
