c38ca0b8e75d2a5a816b4e2e5cf26f39ac145ef4
FhantomWatcher <85093344+FarhanJuneidSiddiqui@users.noreply.github.com>
2024-11-11 23:44:23 +0600

    moved to practice!:)

---
+++ practices/numbertable.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+        int a;
+        cout<<"enter a number for number table: ";
+        cin>>a;
+        cout << a <<"'s number table is given below:- "<<endl;
+
+    for(int i=1;i<11;i++){
+        cout<<a<<"*"<<i<<" = "<< (a*i)<<endl;
+    }
+    return 0;
+}
