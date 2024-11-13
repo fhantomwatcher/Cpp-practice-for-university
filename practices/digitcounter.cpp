5bafcac8003f7958766f1f5fe97c49891067b70e
FhantomWatcher <85093344+FarhanJuneidSiddiqui@users.noreply.github.com>
2024-11-13 18:50:04 +0600

    just optimized the code.:)

--- practices/digitcounter.cpp
+++ practices/digitcounter.cpp
@@ -1,26 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main() {
+    int num, count = 0;
+    cout << "Enter a number to count its digits: ";
+    cin >> num;
 
-    int num,count=0;
-    cout<<"enter a big number to count it's digit:-";
-    cin>>num;
-
-    if(num==0){
-        count=1;
-    }
-    else{
-        if(num<0){
-            num=-num;
-        }
-        while(num>0){
-            num/=10;
+    if (num == 0) {
+        count = 1;
+    } else {
+        num = abs(num); // Convert to positive if negative
+        while (num > 0) {
+            num /= 10;
             count++;
         }
     }
 
-cout<<"digit of the number you enter is "<< count<<endl;
-    
+    cout << "The number of digits is " << count << endl;
     return 0;
 }
